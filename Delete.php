c94ae6efea8e94625471916b362a8fc99088774a
Sang Tran <100749058+stran2312@users.noreply.github.com>
2022-05-02 00:43:29 -0500

    Add files via upload

---
+++ Delete.php
@@ -0,0 +1,53 @@
+<?php
+require_once('connection.php');
+$did=$_REQUEST['did'];
+if (!is_numeric($did) && $did!=NULL)
+{
+	header('Content-Type: application/json');
+	header('HTTP/1.1 200 OK');
+	$output[]="Status: Invalid Data";
+	$output[]="MSG: Device ID must be numbers only.";
+	$output[]="";
+	$responseData=json_encode($output);
+	echo $responseData;
+	die();
+}
+elseif ($did==NULL)
+{
+	header('Content-Type: application/json');
+	header('HTTP/1.1 200 OK');
+	$output[]="Status: Invalid Data";
+	$output[]="MSG: Device ID must not be blank.";
+	$output[]="";
+	$responseData=json_encode($output);
+	echo $responseData;
+	die();
+}
+else
+{
+	$sql="DELETE FROM `devices` WHERE `auto_id`='$did'";
+	$result=$dblink->query($sql) or
+		die("Something went wrong with $sql");
+	if ($result == true)
+	{
+		header('Content-Type: application/json');
+		header('HTTP/1.1 200 OK');
+		$output[]="Status: OK";
+		$output[]="MSG: DELETE SUCCESSFULLY";
+		$output[]="";
+		$responseData=json_encode($output);
+		echo $responseData;
+	}
+	else
+	{
+		header('Content-Type: application/json');
+		header('HTTP/1.1 200 OK');
+		$output[]="Status: Not Found";
+		$output[]="MSG: Device Id: $did not in database";
+		$data[]="";
+		$output[]=$data;
+		$responseData=json_encode($output);
+		echo $responseData;
+	}
+}
+?>
